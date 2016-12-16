d858b27c3823c36719d2f67144e853f1d6287e32
bitlischieber <bitlischieber@users.noreply.github.com>
2016-12-16 15:05:10 +0100

    Drawing on to panels

---
+++ VBnet/Drawing/DrawElement.vb
@@ -0,0 +1,227 @@
+﻿
+
+Public Class DrawElement
+
+    Public Class Dimension
+        Public Property x As Integer
+        Public Property y As Integer
+    End Class
+
+    Public MustInherit Class Element
+        Public Property Pos As Dimension
+        Public Property Size As Dimension
+        Public Property Color As Color
+
+        Public MustOverride Sub Draw(ByRef g As System.Drawing.Graphics, Optional Scaling As Integer = 1)
+
+    End Class
+
+    Public Class Round
+        Inherits Element
+        'Implements IDrawingElement
+
+        Public Sub New(x As Integer, y As Integer, radius As Integer)
+            Me.Pos = New Dimension
+            Me.Pos.x = x
+            Me.Pos.y = y
+            Me.Size = New Dimension
+            Me.Size.x = 2 * radius
+            Me.Size.y = 2 * radius
+            Me.Color = New Color
+            Me.Color = Color.Black
+        End Sub
+
+        Public Overrides Sub Draw(ByRef g As System.Drawing.Graphics, Optional Scaling As Integer = 1)
+            Dim pen As New Pen(Me.Color)
+            g.DrawEllipse(pen, Me.Pos.x * Scaling, Me.Pos.y * Scaling, Me.Size.x * Scaling, Me.Size.y * Scaling)
+        End Sub
+
+    End Class
+
+    Public Class Square
+        Inherits Element
+
+        Public Sub New(x As Integer, y As Integer, s As Integer)
+            Me.Pos = New Dimension
+            Me.Pos.x = x
+            Me.Pos.y = y
+            Me.Size = New Dimension
+            Me.Size.x = s
+            Me.Size.y = s
+            Me.Color = New Color
+            Me.Color = Color.Black
+        End Sub
+
+        Public Overrides Sub Draw(ByRef g As System.Drawing.Graphics, Optional Scaling As Integer = 1)
+            Dim pen As New Pen(Me.Color)
+            g.DrawRectangle(pen, Me.Pos.x * Scaling, Me.Pos.y * Scaling, Me.Size.x * Scaling, Me.Size.y * Scaling)
+        End Sub
+
+    End Class
+
+    Public Class Text
+        Inherits Element
+
+        Dim _string As String
+
+        Public Sub New(x As Integer, y As Integer, Text As String)
+            Me.Pos = New Dimension
+            Me.Pos.x = x
+            Me.Pos.y = y
+            Me.Size = New Dimension
+            Me.Color = New Color
+            Me.Color = Color.Black
+            Me._string = Text
+        End Sub
+
+        Public Overrides Sub Draw(ByRef g As System.Drawing.Graphics, Optional Scaling As Integer = 1)
+            Dim pen As New Pen(Me.Color)
+            Dim font As New Font("Consolas", 10)
+            g.DrawString(_string, font, New SolidBrush(Me.Color), New PointF(Me.Pos.x * Scaling, Me.Pos.y * Scaling))
+        End Sub
+
+    End Class
+
+    Public Class Rectangle
+        Inherits Element
+
+        Public Sub New(x As Integer, y As Integer, length As Integer, height As Integer)
+            Me.Pos = New Dimension
+            Me.Pos.x = x
+            Me.Pos.y = y
+            Me.Size = New Dimension
+            Me.Size.x = length
+            Me.Size.y = height
+            Me.Color = Color.Black
+        End Sub
+
+        Public Overrides Sub Draw(ByRef g As Graphics, Optional Scaling As Integer = 1)
+            Dim pen As New Pen(Me.Color)
+            g.DrawRectangle(pen, Me.Pos.x * Scaling, Me.Pos.y * Scaling, Me.Size.x * Scaling, Me.Size.y * Scaling)
+        End Sub
+
+    End Class
+
+    Public Class Line
+        Inherits Element
+
+        Public Sub New(start_x As Integer, start_y As Integer, end_x As Integer, end_y As Integer)
+            Me.Pos = New Dimension
+            Me.Pos.x = start_x
+            Me.Pos.y = start_y
+            Me.Size = New Dimension
+            Me.Size.x = end_x
+            Me.Size.y = end_y
+            Me.Color = Color.Black
+        End Sub
+
+        Public Overrides Sub Draw(ByRef g As Graphics, Optional Scaling As Integer = 1)
+            Dim pen As New Pen(Me.Color)
+            g.DrawLine(pen, Me.Pos.x * Scaling, Me.Pos.y * Scaling, Me.Size.x * Scaling, Me.Size.y * Scaling)
+        End Sub
+
+    End Class
+
+    Public Class DrawGroup
+
+        Public Group As New List(Of Element)
+
+        Private _name As String
+
+        Public Sub New(Name As String)
+            Me._name = Name
+        End Sub
+
+        Public Function GetMinX() As Integer
+            Dim min As Integer = 0
+            For Each element As Element In Group
+                If (element.Pos.x < min) Then min = element.Pos.x
+            Next
+            Return min
+        End Function
+
+        Public Function GetMinY() As Integer
+            Dim min As Integer = 0
+            For Each element As Element In Group
+                If (element.Pos.y < min) Then min = element.Pos.y
+            Next
+            Return min
+        End Function
+
+        Public Sub TranslateX(XShift As Integer, Optional Scaling As Integer = 1)
+            For Each element As Element In Group
+                element.Pos.x += (XShift * Scaling)
+            Next
+        End Sub
+
+        Public Sub TranslateY(YShift As Integer, Optional Scaling As Integer = 1)
+            For Each element As Element In Group
+                element.Pos.y += (YShift * Scaling)
+            Next
+        End Sub
+
+    End Class
+
+    Public Class Drawing
+        Inherits Dictionary(Of String, DrawGroup)
+
+        Private _graphics As Graphics
+
+        Public Property Scaling As Integer = 1
+
+        Public Sub New(GraphicsHandler As Graphics)
+            _graphics = GraphicsHandler
+        End Sub
+
+        Public Sub New()
+
+        End Sub
+
+        Public Sub SetGraphicsHandler(GraphicsHandler As Graphics)
+            _graphics = GraphicsHandler
+        End Sub
+
+        Public Sub Redraw()
+
+            _graphics.Clear(Color.White)
+            For Each grp As KeyValuePair(Of String, DrawGroup) In Me
+                For Each element In grp.Value.Group
+                    element.Draw(_graphics, Scaling)
+                Next
+            Next
+
+        End Sub
+
+        Public Shadows Sub Add(Key As String, Value As Element)
+
+            If (Not Me.ContainsKey(Key)) Then
+                Dim grp As New DrawGroup(Key)
+                MyBase.Add(Key, grp)
+            End If
+            Me.Item(Key).Group.Add(Value)
+
+        End Sub
+
+        Public Shadows Sub Add(Key As String, Value As DrawGroup)
+            MyBase.Add(Key, Value)
+        End Sub
+
+        Public Sub TranslateX(XShift As Integer, Optional Redraw As Boolean = False)
+            _graphics.Clear(Color.White)
+            For Each grp As KeyValuePair(Of String, DrawGroup) In Me
+                grp.Value.TranslateX(XShift)
+            Next
+            If (Redraw) Then Me.Redraw()
+        End Sub
+
+        Public Sub TranslateY(YShift As Integer, Optional Redraw As Boolean = False)
+            _graphics.Clear(Color.White)
+            For Each grp As KeyValuePair(Of String, DrawGroup) In Me
+                grp.Value.TranslateY(YShift)
+            Next
+            If (Redraw) Then Me.Redraw()
+        End Sub
+
+    End Class
+
+End Class
---
+++ VBnet/Drawing/Form1.vb
@@ -0,0 +1,81 @@
+﻿
+
+Public Class Form1
+
+
+    Private _artwork As DrawElement.Drawing
+
+    Public Sub SetDrawing(ByRef Artwork As DrawElement.Drawing)
+        _artwork = Artwork
+        _artwork.SetGraphicsHandler(pnlDrawing.CreateGraphics)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '_artwork = New DrawElement.Drawing(picDraw.CreateGraphics)
+
+        'Dim art1 As New DrawElement.DrawGroup("art1")
+
+        'Dim element As DrawElement.Element
+        'element = New DrawElement.Round(5, 5, 10)
+        'art1.Group.Add(element)
+        'element = New DrawElement.Square(10, 5, 4)
+        'art1.Group.Add(element)
+        'element = New DrawElement.Line(50, 50, 200, 200)
+        'art1.Group.Add(element)
+
+        '_artwork.Groups.Add(art1)
+
+        '_artwork.Redraw()
+
+        pnlDrawing.AutoScroll = True
+        pnlDrawing.AutoScrollMinSize = New Size(1000, 1000)
+
+        ComboBox1.DataSource = New BindingSource(_artwork, Nothing)
+        ComboBox1.DisplayMember = "Key"
+        ComboBox1.ValueMember = "Value"
+        ComboBox1.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        '_artwork.SetGraphicsHandler(pnlDrawing.CreateGraphics)
+        _artwork.Redraw()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If (Not IsNothing(_artwork)) Then
+            '_artwork.SetGraphicsHandler(picDraw.CreateGraphics)
+            _artwork.Redraw()
+        End If
+
+    End Sub
+
+    Private Sub Panel1_Scroll(sender As Object, e As ScrollEventArgs) Handles pnlDrawing.Scroll
+        pnlDrawing.Invalidate()
+
+        '_artwork.SetGraphicsHandler(picDraw.CreateGraphics)
+        '_artwork.Redraw()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        _artwork.Scaling += 2
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        _artwork.Scaling -= 2
+
+    End Sub
+
+    Private Sub pnlDrawing_Paint(sender As Object, e As PaintEventArgs) Handles pnlDrawing.Paint
+        e.Graphics.TranslateTransform(pnlDrawing.AutoScrollPosition.X, pnlDrawing.AutoScrollPosition.Y)
+    End Sub
+
+    Private Sub pnlDrawing_Click(sender As Object, e As EventArgs) Handles pnlDrawing.Click
+        Dim pos As MouseEventArgs = CType(e, MouseEventArgs)
+        _artwork.TranslateX(pos.X)
+        _artwork.TranslateY(pos.Y, True)
+    End Sub
+End Class
